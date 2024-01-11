42aed861c5c528cce9c18f9ef56d24c7a9fe0952
Rafael Alejandro Barajas Urrutia <82453734+RafaelAlejandro20@users.noreply.github.com>
2024-01-11 15:48:07 -0600

    Actualizacion

--- Rafael_Work/app/src/main/java/com/tesji/rafael_work/MainActivity.kt
+++ Rafael_Work/app/src/main/java/com/tesji/rafael_work/MainActivity.kt
@@ -2,11 +2,28 @@ package com.tesji.rafael_work
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
     val urlBase = "https://rafaelalejandro.pythonanywhere.com/"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl(urlBase)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val service = retrofit.create(PostApi::class.java)
+        lifecycleScope.launch {
+            val response = service.getUserPost()
+            response.forEach(
+                println(it)
+            )
+        }
     }
 }
--- Rafael_Work/app/src/main/java/com/tesji/rafael_work/PostApi.kt
+++ Rafael_Work/app/src/main/java/com/tesji/rafael_work/PostApi.kt
@@ -1,4 +1,8 @@
 package com.tesji.rafael_work
 
+import retrofit2.http.GET
+
 interface PostApi {
+    @GET("empleados")
+    suspend fun getUserPost():ArrayList<PostModel>
 }
